e1e8227ead112df5418ce401ef30b049cc6819f8
Nick Spyrison <spyrison@gmail.com>
2021-02-01 21:14:24 +1100

    app prelimiary rebase.

--- apps/ML_EDA/app.r
+++ apps/ML_EDA/app.r
@@ -1,3 +1,66 @@
+### "Primary" app.R -----
+# options(shiny.error = FALSE)
 
+#' {shiny} application for exploratory data analysis in principal 
+#' component-space. The primary pipeline is to: input data, 
+#' perform preprocessing, estimates the intrinsic data dimensionality,
+#' explore ensemble graphics in PC-space with tooltips back to dataspace. 
+#' Output options will include processed data, PC-space, ensemble graphics
+#' 
+#' @author Nicholas Spyrison
+#' @export
+#' @examples \dontrun{
+#' spinifex::run_app("primary")
+#' }
 
-shiny::runApp()
+#TODO: Initalize code base to ML_EDA use.
+
+source("ui.R", local = TRUE)
+
+server <- function(input, output, session){
+  ### raw_dat
+  raw_dat <- reactive({
+    tourr::flea[]
+  })
+  output$raw_dat_str <- renderPrint({str(raw_dat())})
+  verbatimTextOutput("raw_dat_dty")
+  
+  ### proc_dat 
+  proc_dat <- reactive({
+    raw_dat <- raw_dat()
+    ret <- raw_dat[, apply(raw_dat, MARGIN = 2, is.numeric())]
+    ret <- input$scale_mode(ret)
+    if(input$do_sphere == TRUE) ret <- tourr::sphere_data(ret)
+    retrn(ret)
+  })
+  output$proc_dat_smry <- renderPrint({summary(proc_dat())})
+  
+
+  output$pc_screeplot <- renderPlot({
+    plot(0,0)
+  })
+  
+  output$tour_plot <- renderPlot({
+    plot(0,0)
+  })
+  
+  output$pc_density_plot <- renderPlot({
+    plot(0,0)
+  })
+  
+  output$tsne_plot <- renderPlot({
+    plot(0,0)
+  })
+
+  
+  ### Display to facilitate observing whats going on in the backend.
+  observeEvent(input$dev_browser, {browser()})
+  output$dev_disp <- renderPrint({
+    cat("--- output$Dev_disp --- ",
+        paste0("Other content in paste0()s"),
+        sep = " \n"
+    )
+  })
+}
+
+shinyApp(ui = ui, server = server)
---
+++ apps/ML_EDA/ui.R
@@ -0,0 +1,150 @@
+### "Primary" ui.R -----
+require("spinifex")
+require("tourr")
+require("Rdimtools")
+require("ggplot2")
+require("tibble")
+require("shinythemes") ## Themes for shiny, think preset css styling.
+require("shinyjs")     ## Extend JavaScript (Think HTML interactivity) control and formating, 
+## Also see ?shinyjs::toggle   &   https://daattali.com/shiny/shinyjs-basic/
+##### Additionally used in 'primary' and 'devUnderConstruction':
+require("shinyBS")  ## BootStrap functionality, such as tooltips and popovers
+## Also see ?shinyBS::bsTooltip   &   https://github.com/ebailey78/shinyBS/
+require("DT")       ## HTML tabbles for the gallery table
+
+do_show_dev_disp <- TRUE
+
+
+##### tab1_input -----
+### Input data, default to flea
+tab1_input <- tabPanel("Input", fluidPage(
+    sidebarPanel(width = 3L,
+      ## Input csv file
+      fileInput("data_file", "Data file (.csv format)",
+                accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")
+      ),
+      ## TODO: wrap into raw_dat_dt, really should be converted into a RenderUI
+      ## variables in projection, and target variable
+      checkboxGroupInput("inc_vars",
+                         label = "Projection variables",
+                         choices =  names(tourr::flea[, 1L:6L]),
+                         selected = names(tourr::flea[, 1L:6L])
+      ),
+      radioButtons("aes_var", label = "Color and shape variable",
+                   choices =  names(tourr::flea[]),
+                   selected =  names(tourr::flea[])[7]
+      ),
+      ## Preprocessing
+      radioButtons("scale_mode", "scale data",
+                   choices = c("std dev",
+                               "[0, 1]",
+                               "none"),
+                   selected = "std dev"),
+                   # list("std dev" = spinifex::scale_sd,
+                   #      "[0, 1]"  = spinifex::scale_10,
+                   #      "none"    = function(x){return(x)}),
+                   # selected = spinifex::scale_sd()),
+      checkboxInput("do_sphere", "sphere", 
+                    value = FALSE),
+      ### TODO: these need to have not too many levels, 8 or so?
+      ## Aesthetic selection 
+      # h3("Aesthic options")
+      # fluidRow(column(6L, selectInput("col_var_nm", "Point color", "<none>")),
+      #          column(6L, selectInput("pch_var_nm", "Point shape", "<none>")))
+    ),
+    ## Main panel display
+    mainPanel(h3("Input data"),
+              verbatimTextOutput("raw_dat_str"),
+              h3("Processed data summary"),
+              verbatimTextOutput("proc_dat_smry"),
+    ) ## close mainPanel
+)) ## Assign tab1_input
+
+
+##### tab2_eda ----
+###  Explore PC-space
+tab2_eda <- tabPanel("Explore PC-space", fluidPage(
+  ### Row 1, PC screeplot, tour 
+  fluidRow(
+    ## Left column, screeplot, buttons
+    column(width = 6L,
+           h3("screeplot"),
+           plotOutput("pc_screeplot"),
+           column(width = 6L,
+                  actionButton("sw_less", "< Remove a variable"),
+           ),
+           column(width = 6L,
+                  actionButton("sw_more", "Add a variable >"),
+           ),
+    ), ## Close column, left
+    ## Right column, Stepwise plot, tour mode buttons
+    column(width = 6L,
+           h3("Linear embedding"),
+           plotOutput("tour_plot"),
+           radioButtons("tour_mode", "Tour mode",
+                        choices = c("stepwise",
+                                    "local",
+                                    "grand",
+                        selected = "stepwise",
+                        # list("Stepwise", #= ,
+                        #      "Local", #= 2,
+                        #      "Grand"), # = 3),
+           )
+    ), ## Close column, right
+    ### Row 2
+    fluidRow(
+      ## Left column, pc_density_plot
+      column(width = 6L,
+             h3("PC densities"),
+             plotOutput("pc_density_plot") ## wants + facet_wrap(~var(PC_num))
+      ),
+      ## Left column, pc_density_plot
+      column(width = 6L,
+             h3("None-linnear embedding"),
+             plotOutput("tsne_plot") ## wants + facet_wrap(~var(PC_num))
+      ),
+    )
+  ))) ## Assign tab2_eda
+
+##### tab3_output -----
+tab3_output <- tabPanel("Output", fluidPage(
+  h3("Work in progress"),
+  h2("Thoughts:"),
+  p("-PC space (.rds, .csv)"),
+  p("-processed data (.rds)"),
+  p("-graphics (subset, all?, static?) (.png, .gif?)")
+)) ## Assign tab3_output
+
+##### tab3_output -----
+tab4_about <- tabPanel("About", fluidPage(
+  h3("Work in progress"),
+  h2("Thoughts:"),
+  p("-Context and scope"),
+  p("-ML pyramid/ pipeline"),
+  p("Sources/refferences")
+)) ## Assign tab4_about
+
+##### dev_disp -----
+dev_disp <- if(do_show_dev_disp == T){
+  fluidPage(
+    h3("===== Dev Display below ====="),
+    actionButton("dev_browser", "Run browser()"),
+    verbatimTextOutput("dev_disp")
+  )
+}else{NULL}
+
+###### Full app layout ----
+ui <- fluidPage(theme = shinythemes::shinytheme("flatly"), 
+                ## Esp see the themes: "flatly", "spacelab", "journal"
+                ## Make the lines, hr() black:
+                tags$head(tags$style(HTML("hr {border-top: 1px solid #000000;}"))),
+                shinyjs::useShinyjs(),
+                ## Content:
+                navbarPage("Machine learning exploratory data analysis",
+                           tab1_input,
+                           tab2_eda,
+                           tab3_output,
+                           tab4_about),
+                #h5(contextLine, style = "color: #A9A9A9"),
+                dev_disp
+)
