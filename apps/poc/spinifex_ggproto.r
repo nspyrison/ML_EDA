30b27239e488d61c811826377a2b56b6f9b915e7
Nick Spyrison <spyrison@gmail.com>
2021-05-19 16:31:04 +1000

    ggproto_data_hex working!!! clean up .init_proto

--- apps/poc/spinifex_ggproto.r
+++ apps/poc/spinifex_ggproto.r
@@ -3,10 +3,10 @@ ggplot_tour <- function(basis_array, data = NULL,
                         ## Class/classification for method default values?
 ){
   if(is.null(data) == TRUE)
-    data <- attr(basis_array, "data") ## Also could be NULL.
+    data <- attr(basis_array, "data") ## Can be NULL
   
   ## array2df_version2(), approximately
-  manip_var <- attr(basis_array, "manip_var") ## Only for manual_tour(), else null
+  manip_var <- attr(basis_array, "manip_var") ## NULL if not a manual tour
   if(is.null(manip_var)) ## If not a manual tour interpolate
     basis_array <- tourr::interpolate(basis_array, angle = angle)
   df_ls <- array2df(basis_array, data)
@@ -15,8 +15,8 @@ ggplot_tour <- function(basis_array, data = NULL,
   attr(df_basis, "manip_var") <- manip_var ## NULL if not a manual tour
 
   ## Assign hidden prepared dataframes
-  assign(x = ".spinifex_df_basis",  value = df_basis,  envir = globalenv())
-  assign(x = ".spinifex_df_data",   value = df_data,   envir = globalenv())
+  assign(x = ".spinifex_df_basis", value = df_basis, envir = globalenv())
+  assign(x = ".spinifex_df_data",  value = df_data,  envir = globalenv())
   
   ## Also behave as ggpot() with overwritable theme settings
   ret <- ggplot2::ggplot() +
@@ -28,16 +28,18 @@ ggplot_tour <- function(basis_array, data = NULL,
                    legend.box = "vertical",         ## Between aesthetic
                    legend.margin = margin())        ## Try to minimize margin.
   
-  attr(ret, "class") <- c("ggtour", class(ret))
+  #attr(ret, "class") <- "ggtour"
   return(ret)
 }
 ## Print method
 print.ggtour <- function (x, ...){
+  class(x) <- c("gg", "ggplot")
   x +
     ggproto_basis_axes() +
     ggproto_data_background(gridline_probs = FALSE) +
     ggproto_data_points()
 }
+
 
 ## Initialize common obj from hidden tour objects, test it's existance
 .init_ggproto <- function(){
@@ -45,13 +47,19 @@ print.ggtour <- function (x, ...){
   if(exists(".spinifex_df_basis") == FALSE) 
     stop(paste0("`.spinifex_df_basis` does not exsist, have you run `ggplot_tour` yet?"))
   
-  ## Initialization
-  df_data  <<- .spinifex_df_data
-  df_basis <<- .spinifex_df_basis
-  n_frames <<- length(unique(df_basis$frame))
-  p <<- nrow(df_basis) / n_frames
-  n <<- nrow(df_data)  / n_frames
-  manip_var <<- attr(.spinifex_df_basis, "manip_var") ## NULL if not a manual tour
+  ## Initialization, littering hidden objects 1 level up, not in global.
+  .df_basis <<- .spinifex_df_basis ## Give alterable local copies of _basis and _data
+  .df_data  <<- .spinifex_df_data
+  .n_frames <<- length(unique(.df_basis$frame))
+  .p <<- nrow(.df_basis) / .n_frames
+  .n <<- nrow(.df_data)  / .n_frames
+  .manip_var <<- attr(.df_basis, "manip_var") ## NULL if not a manual tour
+  ## Set .scale_to to df_data else unit box, for spinifex::scale_axes(to = .scale_to)
+  if(is.null(.df_data) == FALSE){
+    .scale_to <<- .df_data
+  }else{
+    .scale_to <<- data.frame(x = c(-1L, 1L), y = c(-1L, 1L))
+  }
 }
 ggproto_basis_axes <- function(position = "left", manip_col = "blue",
                                line_size = 1L, text_size = 5L){
@@ -61,27 +69,23 @@ ggproto_basis_axes <- function(position = "left", manip_col = "blue",
   ## Initialize
   .init_ggproto()
   
+  
   ## Setup and transform
   .angles <- seq(0L, 2L * pi, length = 360L)
   .circle <- data.frame(x = cos(.angles), y = sin(.angles))
-  if(is.null(df_data)){
-    .to <- data.frame(x = c(-1L, 1L), y = c(-1L, 1L))
-  }else{
-    .to <- df_data
-  }
-  .center <- scale_axes(data.frame(x = 0L, y = 0L), position, .to)
-  .circle <- scale_axes(.circle, position, .to)
-  df_basis <- scale_axes(df_basis, position, .to)
+  .center <- scale_axes(data.frame(x = 0L, y = 0L), position, .scale_to)
+  .circle <- scale_axes(.circle, position, .scale_to)
+  .df_basis <- scale_axes(.df_basis, position, .scale_to)
   ## Aesthetics for the axes segments.
   .axes_col <- "grey50"
   .axes_siz <- line_size
-  if (is.null(manip_var) == FALSE) {
-    .axes_col <- rep("grey50", p)
-    .axes_col[manip_var] <- manip_col
-    .axes_col <- rep(.axes_col, n_frames)
-    .axes_siz <- rep(line_size, p)
-    .axes_siz[manip_var] <- 1.5 * line_size
-    .axes_siz <- rep(.axes_siz, n_frames)
+  if (is.null(.manip_var) == FALSE) {
+    .axes_col <- rep("grey50", .p)
+    .axes_col[.manip_var] <- manip_col
+    .axes_col <- rep(.axes_col, .n_frames)
+    .axes_siz <- rep(line_size, .p)
+    .axes_siz[.manip_var] <- 1.5 * line_size
+    .axes_siz <- rep(.axes_siz, .n_frames)
   }
   ## Return ggproto of basis axes in unit circle
   return(
@@ -90,13 +94,13 @@ ggproto_basis_axes <- function(position = "left", manip_col = "blue",
                          size = line_size, inherit.aes = FALSE,
                          mapping = ggplot2::aes(x = x, y = y)),
         suppressWarnings(ggplot2::geom_segment( ## Suppress unused arg: frames
-          data = df_basis,
+          data = .df_basis,
           size = .axes_siz, colour = .axes_col,
           mapping = ggplot2::aes(x = x, y = y, frame = frame,
                                  xend = .center[, 1L], yend = .center[, 2L])
         )),
       suppressWarnings(ggplot2::geom_text(
-        data = df_basis,
+        data = .df_basis,
         colour = .axes_col, size = text_size,
         vjust = "outward", hjust = "outward",
         mapping = ggplot2::aes(x = x, y = y, frame = frame, label = label)
@@ -121,11 +125,10 @@ ggproto_data_background <- function(zero_mark = TRUE,
      all(gridline_probs >= 0L) &
      all(gridline_probs <= 1L)
   ){
-    .to <- df_data
-    .x_min <- min(.to[, 1L])
-    .y_min <- min(.to[, 2L])
-    .x_max <- max(.to[, 1L])
-    .y_max <- max(.to[, 2L])
+    .x_min <- min(.scale_to[, 1L])
+    .y_min <- min(.scale_to[, 2L])
+    .x_max <- max(.scale_to[, 1L])
+    .y_max <- max(.scale_to[, 2L])
     .x_q <- .x_min + gridline_probs * diff(range(.x_min, .x_max))
     .y_q <- .y_min + gridline_probs * diff(range(.y_min, .y_max))
     .len <- length(gridline_probs)
@@ -138,7 +141,7 @@ ggproto_data_background <- function(zero_mark = TRUE,
     
     gridlines <- ggplot2::geom_segment(
       data = .df_gridlines,
-      color = "grey80", size = .5,
+      color = "grey80", size = .5, alpha = .5,
       mapping = ggplot2::aes(x = x, y = y, xend = x_end, yend = y_end)
     )
     ret <- c(ret, gridlines)
@@ -146,7 +149,7 @@ ggproto_data_background <- function(zero_mark = TRUE,
   
   #### Setup zero mark, 5% on each side.
   if(zero_mark == TRUE){
-    .center <- scale_axes(data.frame(x = 0L, y = 0L), position, .to)
+    .center <- scale_axes(data.frame(x = 0L, y = 0L), position, .scale_to)
     .x_tail <- .05 * diff(range(.x_min, .x_max))
     .y_tail <- .05 * diff(range(.y_min, .y_max))
     .df_zero_mark <-
@@ -158,7 +161,7 @@ ggproto_data_background <- function(zero_mark = TRUE,
     
     zero_mark <- ggplot2::geom_segment(
       data = .df_zero_mark,
-      color = "grey60", size = 1L,
+      color = "grey60", size = 1L, alpha = .7,
       mapping = ggplot2::aes(x = x, y = y, xend = x_end, yend = y_end)
     )
     ret <- c(ret, zero_mark)
@@ -168,6 +171,17 @@ ggproto_data_background <- function(zero_mark = TRUE,
   return(ret)
 }
 
+#' @examples
+#' dat <- scale_sd(mtcars)
+#' bas <- basis_pca(dat)
+#' gt_array <- save_history(dat, grand_tour(), max = 3, start = bas)
+#' 
+#' gg <- ggplot_tour(gt_array, dat) +
+#'   ggproto_basis_axes() +
+#'   ggproto_data_points() +
+#'   ggproto_data_hex()
+#' 
+#' animate_gganimate(gg)
 ggproto_data_points <- function(aes_args = list(),
                                 identity_args = list()){
   ## Assumptions
@@ -180,21 +194,21 @@ ggproto_data_points <- function(aes_args = list(),
   .init_ggproto()
   
   ## Add aes_args to df_data, replicating across frame
-  .tgt_len  <- nrow(df_data)
-  .orig_nms <- names(df_data)
+  .tgt_len  <- nrow(.df_data)
+  .orig_nms <- names(.df_data)
   .aes_arg_nms <- names(aes_args)
   .mute <- lapply(seq_along(aes_args), function(i){
     .this_arg <- aes_args[[i]]
-    if(length(.this_arg) %in% c(1L, n) == FALSE)
+    if(length(.this_arg) %in% c(1L, .n) == FALSE)
       warning(paste0("aes_arg '", .aes_arg_nms[i], "' not of length 1 or data."))
     .this_col <- rep_len(.this_arg, .tgt_len)
-    df_data[, ncol(df_data) + 1L] <<- .this_col ## Bind column to df_data
+    .df_data[, ncol(.df_data) + 1L] <<- .this_col ## Bind column to df_data
     aes_args[[i]] <<- .this_col ## Replace the value with the string of the orig
     ## TODO Warning this may cause issues down the line!?
     #### as ggplot will want to treat this as a vector rather than column of the df?
     #### Causes the split in the legend... i don't see a way around it, without having end
   })
-  names(df_data) <- c(.orig_nms, .aes_arg_nms) ## Repair names
+  names(.df_data) <- c(.orig_nms, .aes_arg_nms) ## Repair names
   
   ## do.call aes() over the aes_args
   .aes_func <- function(...)
@@ -203,7 +217,7 @@ ggproto_data_points <- function(aes_args = list(),
   ## do.call geom_point() over the identity_args
   .geom_func <- function(...){
     suppressWarnings(
-      ggplot2::geom_point(mapping = .aes_call, data = df_data, ...)
+      ggplot2::geom_point(mapping = .aes_call, data = .df_data, ...)
     )
   }
   .geom_call <- do.call(.geom_func, identity_args)
@@ -225,22 +239,22 @@ ggproto_data_text <- function(aes_args = list(label = as.character(1:nrow(dat)))
   .init_ggproto()
   
   ## Add aes_args to df_data, replicating across frame
-  .tgt_len   <- nrow(df_data)
-  .orig_nms <- names(df_data)
+  .tgt_len   <- nrow(.df_data)
+  .orig_nms <- names(.df_data)
   .aes_arg_nms <- names(aes_args)
   .mute <- lapply(seq_along(aes_args), function(i){
     .this_arg <- aes_args[[i]]
-    if(length(.this_arg) %in% c(1L, n) == FALSE)
+    if(length(.this_arg) %in% c(1L, .n) == FALSE)
       warning(paste0("aes_arg '", .aes_arg_nms[i], "' not of length 1 or data."))
     .this_col <- rep_len(.this_arg, .tgt_len)
-    df_data[, ncol(df_data) + 1L] <<- .this_col ## Bind column to df_data
+    .df_data[, ncol(.df_data) + 1L] <<- .this_col ## Bind column to df_data
     aes_args[[i]] <<- .this_col ## Replace the value with the string of the orig
     ## TODO Warning this may cause issues down the line!?
     #### as ggplot will want to treat this as a vector rather than column of the df?
     #### Causes the split in the legend... i don't see a way around it, without having end
   })
   ## repair names
-  names(df_data) <- c(.orig_nms, .aes_arg_nms)
+  names(.df_data) <- c(.orig_nms, .aes_arg_nms)
   
   ## do.call aes() over the aes_args 
   .aes_func <- function(...)
@@ -257,12 +271,23 @@ ggproto_data_text <- function(aes_args = list(label = as.character(1:nrow(dat)))
   return(.geom_call)
 }
 
-## Printing as points 
-ggproto_data_hdrcde <- function(aes_args = list(label = as.character(1:nrow(dat))),
-                              identity_args = list()){
+#' @examples
+#' dsmall <- diamonds[sample(nrow(diamonds), 1000), c(1, 5:6, 8:10)]
+#' dat <- scale_sd(dsmall)
+#' bas <- basis_pca(dat)
+#' gt_array <- save_history(dat, grand_tour(), max = 3, start = bas)
+#' 
+#' ggp <- ggplot_tour(gt_array, dat) +
+#'   ggproto_basis_axes() +
+#'   ggproto_data_hex() +
+#'   ggproto_data_background()
+#' animate_gganimate(ggp)
+## Printing as points
+ggproto_data_hex <- function(bins = 30,
+                             aes_args = list(),
+                             identity_args = list()){
   ## Assumptions
   if(is.null(.spinifex_df_data) == TRUE) return()
-  requireNamespace("hdrcde")
   position <- "center" ## Data assumed center.
   aes_args <- as.list(aes_args)
   identity_args <- as.list(identity_args)
@@ -271,34 +296,35 @@ ggproto_data_hdrcde <- function(aes_args = list(label = as.character(1:nrow(dat)
   .init_ggproto()
   
   ## Add aes_args to df_data, replicating across frame
-  .tgt_len   <- nrow(df_data)
-  .orig_nms <- names(df_data)
+  .tgt_len   <- nrow(.df_data)
+  .orig_nms <- names(.df_data)
   .aes_arg_nms <- names(aes_args)
   .mute <- lapply(seq_along(aes_args), function(i){
     .this_arg <- aes_args[[i]]
-    if(length(.this_arg) %in% c(1L, n) == FALSE)
+    if(length(.this_arg) %in% c(1L, .n) == FALSE)
       warning(paste0("aes_arg '", .aes_arg_nms[i], "' not of length 1 or data."))
     .this_col <- rep_len(.this_arg, .tgt_len)
-    df_data[, ncol(df_data) + 1L] <<- .this_col ## Bind column to df_data
+    .df_data[, ncol(.df_data) + 1L] <<- .this_col ## Bind column to df_data
     aes_args[[i]] <<- .this_col ## Replace the value with the string of the orig
     ## TODO Warning this may cause issues down the line!?
     #### as ggplot will want to treat this as a vector rather than column of the df?
     #### Causes the split in the legend... i don't see a way around it, without having end
   })
   ## repair names
-  names(df_data) <- c(.orig_nms, .aes_arg_nms)
+  names(.df_data) <- c(.orig_nms, .aes_arg_nms)
   
-  ## do.call aes() over the aes_args 
+  ## do.call aes() over the aes_args
   .aes_func <- function(...)
-    ggplot2::aes(x = x, y = y, frame = frame, ...)
+    ggplot2::aes(x = x, y = y, frame = frame, group = frame, ...)
   .aes_call <- do.call(.aes_func, aes_args)
-  ## do.call geom_point() over the identity_args 
+  ## do.call geom_point() over the identity_args
   .geom_func <- function(...){
     suppressWarnings(
-      ggplot2::geom_text(mapping = .aes_call, data = df_data, ...)
+      ggplot2::geom_hex(mapping = .aes_call, data = .df_data, bins = bins, ...)
     )
   }
   .geom_call <- do.call(.geom_func, identity_args)
+  
   ## Return ggproto
   return(.geom_call)
 }
@@ -313,7 +339,7 @@ if(interactive()){
   require("hdrcde")
 }
 ##  GGPLOT RETURN BEFORE ANIM
-#' @examples 
+#' @examples
 #' dat <- scale_sd(mtcars)
 #' bas <- basis_pca(dat)
 #' gt_array <- save_history(dat, grand_tour(), max = 3, start = bas)
@@ -321,7 +347,7 @@ if(interactive()){
 #' ggplot_tour(gt_array, dat) +
 #'   ggproto_basis_axes() +
 #'   ggproto_data_background() +
-#'   ggproto_data_points() + theme(legend.position = "right")
+#'   ggproto_data_points()
 #' 
 #' dat <- scale_sd(tourr::flea[, 1:6])
 #' clas <- flea[, 7]
@@ -337,7 +363,7 @@ if(interactive()){
 #'   ggproto_data_text()
 
 ## TESTING GGPLOT RETURN BEFORE ANIM
-if(F) ?render_gganimate 
+if(F) ?render_gganimate
 #' @examples 
 #' dat <- scale_sd(::flea[, 1:6])
 #' clas <- flea[, 7]
@@ -357,7 +383,7 @@ if(F) ?render_gganimate
 #' (anim <-
 #'   animate_gganimate(ggtour, fps = 10, rewind = TRUE,
 #'                     start_pause = 1, end_pause = 2))
-#'                            
+#' 
 #' gganimate::anim_save("my_tour.gif",
 #'                      animation = anim,
 #'                      path = "./figures")
@@ -368,7 +394,8 @@ animate_gganimate <- function(
   ... ## Passed to gganimate::animate or gganimate::knit_print.gganim
   ){
   requireNamespace("gganimate")
-  gga <- ggtour + gganimate::transition_states(frame, transition_length = 0L)
+  gga <- ggtour +
+    gganimate::transition_states(frame, transition_length = 0L)
   
   ## Pdf animation, early return
   if(knit_pdf_anim == TRUE){
@@ -410,12 +437,12 @@ if(F) ?render_plotly
 #'                         selfcontained = TRUE)
 #' }
 animate_plotly <- function(
-  ggtour, fps = 8L, ##tooltip = "none",
+  ggtour, fps = 8L, #use_rowname_tooltip = TRUE,
   ... ## Passed to plotly::layout.
 ){
   requireNamespace("plotly")
-  ggp <- plotly::ggplotly(p = ggtour, tooltip = tooltip)
-  ggp <- plotly::animation_opts(p = ggp, frame = 1L/fps * 1000L,
+  ggp <- plotly::ggplotly(p = ggtour)
+  ggp <- plotly::animation_opts(p = ggp, frame = 1L / fps * 1000L,
                                 transition = 0L, redraw = FALSE)
   ggp <- plotly::layout(
     ggp, 
--- zDev/hdecde.r
+++ zDev/hdecde.r
@@ -1,5 +1,6 @@
 require(hdrcde)
 
+#### hdrcde::hdrscatterplot ---- 
 ?hdrcde::hdrscatterplot
 
 x <- c(rnorm(200, 0, 1), rnorm(200, 4, 1))
@@ -8,6 +9,59 @@ hdrscatterplot(x, y)
 hdrscatterplot(x, y, label = paste0("p", 1:length(x)))
 hdrcde::hdrscatterplot(x, y)
 
+print("would like something like this, but I think geom_density_2d will be an easier first step.")
+
+
+require("ggplot2")
+require("tictoc")
+?geom_density2d
+?geom_hex
+if(F)
+  browseURL("https://www.r-graph-gallery.com/2d-density-plot-with-ggplot2.html")
+
+# Show the contour only
+# Data
+a <- data.frame( x=rnorm(20000, 10, 1.9), y=rnorm(20000, 10, 1.2) )
+b <- data.frame( x=rnorm(20000, 14.5, 1.9), y=rnorm(20000, 14.5, 1.9) )
+c <- data.frame( x=rnorm(20000, 9.5, 1.9), y=rnorm(20000, 15.5, 1.9) )
+data <- rbind(a,b,c)
+
+{
+tic()
+ggplot(data, aes(x=x, y=y) ) +
+  geom_density_2d()
+toc()
+}{
+tic()
+# Show the area only
+ggplot(data, aes(x=x, y=y) ) +
+  stat_density_2d(aes(fill = ..level..), geom = "polygon")
+toc()
+}{
+tic()
+# Area + contour
+ggplot(data, aes(x=x, y=y) ) +
+  stat_density_2d(aes(fill = ..level..), geom = "polygon", colour="white")
+toc()
+}
+
+{ 
+  tic()
+  # Area + contour
+  hdrcde::hdrscatterplot(data$x, data$y)
+  toc()
+}
+
+{ 
+  tic()
+  # Area + contour
+  ggplot(data, aes(x=x, y=y) ) +
+    geom_hex()
+  toc()
+}
+
+
+#### hdrcde::cde, not promising ---- 
 if(F){
   print("Nahhh, I think I just want HDR.")
   ?cde
