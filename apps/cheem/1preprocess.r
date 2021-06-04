d7738de2eaece3a4e18630147877c9dce1072f19
Nick Spyrison <spyrison@gmail.com>
2021-06-04 12:35:20 +1000

    preprocess and artifacts.

--- apps/cheem/1preprocess.r
+++ apps/cheem/1preprocess.r
@@ -1,9 +1,15 @@
+## Creates objs for use in shiny app:
+if(F){
+  dat
+  tgt_var
+  maha_lookup_df
+}
+
 ## Created from analysis in:
 if(F){
   file.edit("./vignettes/cheem_fifa.rmd")
   file.edit("./vignettes/cheem_varieties.rmd")
 }
-
 
 ## Dependencies ------
 require("DALEX")
@@ -21,25 +27,27 @@ if(F){ ## Manually run to view file:
 }
 
 ## Setup ------
-raw <- DALEX::fifa
-str(raw)
-## Remove factor, target variable `overall`, and highly correlated var, `potential`
-dat <- raw %>% dplyr::select(-c(`nationality`)) ##, `potential`, `overall`))
-## I know there are too many features, so we'll also bemove some correlatted variables.
-tic("correlation")
+.raw <- DALEX::fifa
+## I know there are too many features, so we'll remove some correlated variables.
 
-## Correlation embedding -----
-cor_mat <- cor(dat)
-corrplot::corrplot(cor_mat,
-                   method = "circle", ## geom
-                   type = "upper", ## only upper triangle
-                   diag = F, ## remove auto correlation
-                   order = "FPC", ## First principal component
-                   tl.col = "black", tl.srt = 90, ## Text label color and rotation
-                   tl.pos = "td")
-toc()
-## Further avg some highly correlated vars.
-dat <- dat %>% dplyr::mutate(
+if(F){
+  ## Correlation embedding -----
+  str(.raw)
+  tic("correlation")
+  cor_mat <- .raw %>% dplyr::select(-c(`nationality`)) %>% 
+    cor()
+  corrplot::corrplot(cor_mat,
+                     method = "circle", ## geom
+                     type = "upper", ## only upper triangle
+                     diag = F, ## remove auto correlation
+                     order = "FPC", ## First principal component
+                     tl.col = "black", tl.srt = 90, ## Text label color and rotation
+                     tl.pos = "td")
+  toc()
+}
+
+## Agg some highly correlated vars.
+dat <- .raw %>% dplyr::select(-c(`nationality`)) %>% dplyr::mutate(
   .keep = "unused",
   ## Target variables, agg skill, and financial 
   value = (potential+overall+wage_eur+value_eur+movement_reactions)/5,
@@ -64,20 +72,8 @@ dat <- dat %>% dplyr::mutate(
   gk = (goalkeeping_diving+goalkeeping_positioning+goalkeeping_reflexes+
           goalkeeping_handling+goalkeeping_kicking)/5,
 )
-## starting with 42 variables, we remove 1, and 
+## starting with 42 variables, we remove `nationality`, and 
 ## aggregate the other 41 variables into 9 aggregate dimensions based on like correlation of variable, including `value` our tgt variable.
-if(F){
-  dim(raw)
-  dim(dat)
-  cor_mat <- cor(dat)
-  corrplot::corrplot(cor_mat,
-                     method = "circle", ## geom
-                     type = "upper", ## only upper triangle
-                     diag = F, ## remove auto correlation
-                     order = "FPC", ## First principal component
-                     tl.col = "black", tl.srt = 90, ## Text label color and rotation
-                     tl.pos = "td")
-}
 
 ## Normalize each column by its standard deviations
 dat <- spinifex::scale_sd(dat) %>% as.data.frame()
@@ -85,12 +81,12 @@ tgt_var <- dat$value
 dat <- dat %>% select(-value)
 
 ## Mahalonobis lookup ----
-maha_dist <-
+.maha_dist <-
   mahalanobis(dat, colMeans(dat), cov(dat)) %>%
   sort(decreasing = TRUE)
 maha_lookup_df <- data.frame(id = 1:nrow(dat),
                              name = rownames(dat),
-                             dist = maha_dist,
+                             dist = .maha_dist,
                              value = tgt_var,
                              bmi = dat$bmi,
                              age = dat$age,
@@ -101,13 +97,23 @@ maha_lookup_df <- data.frame(id = 1:nrow(dat),
                              pwr = dat$pwr,
                              gk  = dat$gk)
 
-## local_attribution_list -----
 
-tictoc::tic("local_attribution_list")
-#cheem <- basis_cheem(dat[1:500,], 42, tgt_var[1:500,])
-la_ls <- local_attribution_list(dat, tgt_var)
-tictoc::toc()
 
+## RF model
+
+basis_cheem(data = dat, holdout_rownum = tgt_row, target_var = y_var,
+           parts_type = "shap", basis_type = "pca")
+
+## local_attribution_list NOT RUN -----
+if(F){
+  tictoc::tic("local_attribution_list")
+  la_ls <- local_attribution_list(dat, tgt_var)
+  tictoc::toc()
+  ### Failing in the loop; DALEX::predict_parts? not when manually run, may want to try  running outside of doParallel, with reduced RFs...
+  # Error in { : 
+  #     task 1 failed - "no applicable method for 'predict' applied to an object of class "c('randomForest.formula', 'randomForest')""
+  str(la_ls)
+}
 
 
 if(F){
@@ -118,10 +124,20 @@ if(F){
   object.size(cheem)
   
   ## 3.8 Hrs for 5000x30sec / 11 cores
-  ## 27.8 Gb for  694736 bytes * 5000 in Gb
-  ## .67 Gb  for 16912 bytes * 5000 in Gb
-  z<- cheem
+  ## 27.8 Gb for 694736 bytes * 5000 in Gb
+  ## .67 Gb  for  16912 bytes * 5000 in Gb
+  z <- cheem
   attr(z, "data_else") <- NULL
   str(z)
   object.size(z)
+  
+  
+  for(i in c(1, 3, 7, 10)){
+    tictoc::tic(paste0("B = ",i))
+    print(basis_cheem(dat, 42, tgt_var, parts_B = i))
+    tictoc::toc()
+  }
+  
+  
 }
+
