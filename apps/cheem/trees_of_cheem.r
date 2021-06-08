aee4bc75527f1f1c44c379a594c731edf9fae9eb
Nick Spyrison <spyrison@gmail.com>
2021-06-08 20:45:43 +1000

    perfromance benchmark and treeshap_df()

--- apps/cheem/trees_of_cheem.r
+++ apps/cheem/trees_of_cheem.r
@@ -1,5 +1,6 @@
-require("DALEX")
+require("ranger")
 require("treeshap")
+require("DALEX")
 require("spinifex")
 require("tourr")
 require("ggplot2")
@@ -15,6 +16,36 @@ draw_observation <- function(data){
   return(obs)
 }
 
+#' @examples
+#' dat <- DALEX::apartments[, 1:5]
+#' xdat <- dat[, 2:5] ## -c(m2.price, district))
+#' y <- dat$m2.price
+#' 
+#' ## Fit a {randomForest} model, slower fit, but faster shap than ranger
+#' .rf <- ranger::ranger(y ~ ., data = data.frame(y, xdat))
+#'  df_shap <- treeshap_df(.rf, data = xdat)
+treeshap_df <- function(randomForest_model, data){
+  .rfu <- treeshap::randomForest.unify(.rf, xdat)
+  .tshap_ls <- treeshap::treeshap(.rfu, x = xdat) 
+  .tshap_ls <- .tshap_ls[c(1,4)]
+  ## Keeping only c(1,4); reduces ~98.5% of the obj size, keep shap values make data attr.
+  ## But, we lose the iBreakdown-like plot of treeshap::plot_contribution when we take this apart.
+  ret <- .tshap_ls[[1]]
+  
+  attr(ret, "class") <- c("treeshap_df", "data.frame")
+  attr(ret, "data")  <- .tshap_ls[[2]] ## Also a data.frame
+  return(ret)
+}
+## Print basis_cheem_INSAMP as a numeric matrix without showing all the attributes.
+print.treeshap_df <- function (x, ...){
+  attr(x, "data") <- NULL
+  NextMethod()
+}
+
+
+# dist_mat <- as.matrix(dist(df_shap))
+
+
 
 #' @examples
 #' ## Discrete supervised classification RF
@@ -25,7 +56,7 @@ draw_observation <- function(data){
 #' GGally::ggpairs(as.data.frame(la_mat), mapping = aes(color = tgt_var))
 ## Assumes pkg "doParallel", see fastshap, and treeshap....
 local_attribution_matrix <- function(
-  data, target_var,
+  data, y,
   parts_type = c("shap", "break_down", "oscillations", "oscillations_uni", "oscillations_emp"),
   parts_B = 10,
   parts_N = if(substr(parts_type, 1, 4) == "osci") 500 else NULL, ## see DALEX::predict_parts
@@ -263,255 +294,3 @@ autoplot.basis_cheem <- plot.basis_cheem <- function(
 }
 
 
-
-
-
-#### OLD METHOD, BEING DEPRICATED -----
-
-#' #' @example
-#' #' ## Discrete supervised classification
-#' #' dat <- spinifex::scale_sd(tourr::flea[, 1:6])
-#' #' clas <- flea$species
-#' #' tgt_obs <- 10 ## row number in 1:nrow(dat)
-#' #' tgt_var <- clas == clas[tgt_obs] ## Or regression on a continuous var not in data.
-#' #' 
-#' #' basis_cheem(dat, tgt_obs, tgt_var, clas, basis_type = NULL)
-#' #' 
-#' #' ## Continuous regression, no class aesthetics
-#' #' dat <- dplyr::select(DALEX::fifa, -c("nationality", "potential", "value_eur", "wage_eur"))
-#' #' set.seed(123)
-#' #' .r_idx <- sample(1:nrow(dat), 1000)
-#' #' dat <- dat[.r_idx, ]
-#' #' dat <- scale_sd(dat)
-#' #' y_var <- DALEX::fifa$overall[.r_idx] ## An aggregate skill measure between 1 and 100.
-#' #' 
-#' #' #### Find a outlier to look at
-#' #' .maha <- mahalanobis(dat, colMeans(dat), cov(dat))
-#' #' .maha <- sort(.maha, decreasing = T)
-#' #' head(.maha, n = 6L) ## "I. Pettersson", 4th largest mahalonobis dist, google: a Sweddish goalkepper
-#' #' .tgt_name <- "I. Pettersson"
-#' #' tgt_row <- which(row.names(dat) == .tgt_name)
-#' #' 
-#' #' basis_cheem(data = dat, holdout_rownum = tgt_row, target_var = y_var,
-#' #'             parts_type = "shap", basis_type = "pca")
-#' ## Previously hard coded classification target var was: class == new_observation_class
-#' basis_cheem <- function(
-#'   data, holdout_rownum, target_var, class = NULL,
-#'   parts_type = c("shap", "break_down", "oscillations", "oscillations_uni", "oscillations_emp"),
-#'   parts_B = 10,
-#'   parts_N = if(substr(parts_type, 1, 4) == "osci") 500 else NULL, ## see DALEX::predict_parts
-#'   basis_type = c("none", NULL, "RF_importance", "pca", "olda", "odp", "onpp"),
-#'   keep_large_intermediates = FALSE,
-#'   ...
-#' ){
-#'   ## Assumptions
-#'   requireNamespace("randomForest")
-#'   requireNamespace("DALEX")
-#'   #requireNamespace("treeshap") ## Not explicitly used, maybe implicitly called in DALEX?
-#'   data <- as.data.frame(data)
-#'   ## Initialize held out observation for data, target var, optional class var
-#'   data_oos  <- data[holdout_rownum,, drop = FALSE] ## drop = FALSE retains data.frame rather than coerce to vector.
-#'   data_else <- data[-holdout_rownum, ]
-#'   target_var_oos  <- target_var[holdout_rownum]
-#'   target_var_else <- target_var[-holdout_rownum]
-#'   
-#'   ## If class is used
-#'   class_oos <- class_else <- NULL ## Initialize
-#'   if(is.null(class) == FALSE){
-#'     class <- as.factor(class)
-#'     class_oos  <- class[holdout_rownum]
-#'     class_else <- class[-holdout_rownum]
-#'   }else{
-#'     ## If class is null, enforce correct basis functions
-#'     if(is.null(basis_type) == FALSE){
-#'       basis_type <- match.arg(basis_type)
-#'       if(basis_type %in% c("olda", "odp"))
-#'         stop(paste0("basis_type ", basis_type, " requires the 'class' argument."))
-#'     }
-#'   }
-#'   
-#'   #### Random forest, with holdout_rownum removed
-#'   .p <- ncol(data)
-#'   ## Discrete wants mtry = sqrt(p), continuous wants mtry = p/3
-#'   .RF_mtry <- if(is.discrete(target_var_else)) sqrt(.p) else .p / 3L
-#'   .do_clac_imp <- FALSE ## Init
-#'   if(!is.null(basis_type)) if(basis_type == "RF_importance") .do_clac_imp <- TRUE
-#'   .rf <- randomForest::randomForest(target_var_else~.,
-#'                                     data = data.frame(target_var_else, data_else),
-#'                                     mtry = .RF_mtry,
-#'                                     importance = .do_clac_imp)
-#'   
-#'   #### DALEX::predict_parts, (of DALEX::explain()) of that Random forest
-#'   parts_type <- match.arg(parts_type)
-#'   .ex_rf <- DALEX::explain(
-#'     model = .rf,
-#'     data = data_else,
-#'     y = target_var_else,
-#'     label = paste0(parts_type, " local attribution of random forest model"))
-#'   .parts <- DALEX::predict_parts(explainer = .ex_rf, ## Takes some time.
-#'                                  new_observation = data_oos,
-#'                                  type = parts_type,
-#'                                  N = parts_N,
-#'                                  B = parts_B,
-#'                                  ...)
-#'   if(keep_large_intermediates == FALSE)
-#'     attr(.parts, "yhats_distribution") <- NULL ## remove ~90% the size, without hurting plot.predict_parts
-#'   
-#'   #### The local attribution of those parts [1, p] vector in SHAP order.
-#'   ## Remade from: iBreakDown:::print.break_down_uncertainty
-#'   .df_la <- data.frame(
-#'     label = tapply(.parts$label, paste(.parts$label, .parts$variable, sep = ": "), unique, na.rm = TRUE),
-#'     variable_name = tapply(.parts$variable_name, paste(.parts$label, .parts$variable, sep = ": "), unique, na.rm = TRUE),
-#'     variable_value = tapply(.parts$variable_value, paste(.parts$label, .parts$variable, sep = ": "), unique, na.rm = TRUE), ## oos variable value
-#'     median_local_attr = tapply(.parts$contribution, paste(.parts$label, .parts$variable, sep = ": "), median, na.rm = TRUE)
-#'   )
-#'   ## Reorder .df_la back to original data colname order
-#'   .row_idx <- order(match(.df_la$variable_name, colnames(data)))
-#'   .df_la   <- .df_la[.row_idx, ]
-#'   
-#'   # .la_r_fct_lvl <- gsub(".*\\.","", .parts$label)
-#'   # .fct_lvl <- if(length(unique(.la_r_fct_lvl)) == 1L){""}else{.la_r_fct_lvl}
-#'   ## What if factor was tested instead of boolean?
-#'   .guess_bas.dat_ratio <- nrow(.df_la) / ncol(data)
-#'   if(.guess_bas.dat_ratio != 1L)
-#'     stop(paste0(
-#'       "Local attribution has ", .guess_bas.dat_ratio,
-#'       " times as many rows as the columns of the data. Make sure you didn't want to test a boolean such as tgt_var <- clas == clas[new_obs]. If you need this loop over a test for each factor level."))
-#'   # paste0(substitute(class), "=", gsub(".*\\.","", .parts$label))
-#'   
-#'   #### Basis of a global feature (holdout_rownum removed), to use as second dim of projection
-#'   if(is.null(basis_type) == FALSE){
-#'     if(basis_type != "none"){
-#'       basis_type <- match.arg(basis_type)
-#'       .bas <- switch(basis_type,
-#'                      RF_importance = randomForest::importance(.rf, type = 1L),
-#'                      pca  = basis_pca(data_else),
-#'                      olda = basis_olda(data_else, class_else),
-#'                      odp  = basis_odp(data_else, class_else),
-#'                      onpp = basis_onpp(data_else), ## Using default hyperparameters
-#'                      stop("basis_type expects 'RF_importance', 'pca', 'olda', 'odp', 'onpp' or NULL.")
-#'       )
-#'       
-#'       #### Bring them together and orthonormalize
-#'       .cheem_bas <- cbind(.df_la$median_local_attr, .bas)[, 1L:2L]
-#'       .cheem_bas <- as.matrix(tourr::orthonormalise(.cheem_bas))
-#'       colnames(.cheem_bas) <- c(parts_type, paste0(basis_type, "1"))
-#'     }
-#'   }else{ ## basis_type is NULL; format parts local attributes
-#'     .cheem_bas <- as.matrix(tourr::normalise(.df_la$median_local_attr))
-#'     colnames(.cheem_bas) <- parts_type
-#'     rownames(.cheem_bas) <- .df_la$variable_name
-#'   }
-#'   
-#'   ## Keep attributes
-#'   attr(.cheem_bas, "class") <- c("cheem_basis", "matrix")
-#'   attr(.cheem_bas, "data_else") <- as.matrix(data_else)
-#'   attr(.cheem_bas, "data_oos")  <- as.matrix(data_oos)
-#'   attr(.cheem_bas, "class_else") <- class_else ## Can't call it "class" b/c matrix/df.
-#'   attr(.cheem_bas, "class_oos")  <- class_oos
-#'   attr(.cheem_bas, "predict_parts") <- .parts
-#'   if(keep_large_intermediates == TRUE){
-#'     attr(.cheem_bas, "randomForest") <- .rf
-#'     attr(.cheem_bas, "explain")      <- .ex_rf
-#'   }
-#'   
-#'   ## Return
-#'   return(.cheem_bas)
-#' }
-#' 
-#' ## Print cheem_bases as a numeric matrix without showing all the attributes.
-#' print.cheem_basis <- function (x, ...){
-#'   attr(x, "data_else") <- NULL
-#'   attr(x, "data_oos")  <- NULL
-#'   attr(x, "class_else") <- NULL
-#'   attr(x, "class_oos")  <- NULL
-#'   attr(x, "randomForest")  <- NULL
-#'   attr(x, "explain")       <- NULL
-#'   attr(x, "predict_parts") <- NULL
-#'   NextMethod()
-#' }
-#' 
-#' 
-#' #' @example
-#' #' dat <- spinifex::scale_sd(flea[, 1:6])
-#' #' clas <- flea$species
-#' #' tgt_obs <- 10 ## row number in 1:nrow(dat)
-#' #' tgt_var <- clas == clas[tgt_obs] ## Or regression on a continuous var not in data.
-#' #' 
-#' #' bas_cheem <- basis_cheem(dat, tgt_obs, tgt_var, clas, basis_type = "olda")
-#' #' 
-#' #' (ggcheem_proj <-
-#' #'   view_cheem(bas_cheem))
-#' #' 
-#' #' view_cheem(bas_cheem, show_boxplots = FALSE, max_features = 4)
-#' #' 
-#' #' if(F)
-#' #'   ggsave("PoC_view_cheem.pdf", ggcheem_proj, device ="pdf", width = 6, height = 3, units="in")
-#' view_cheem <- autoplot.cheem_basis <- plot.cheem_basis <- function(
-#'   cheem_basis, show_parts = TRUE,
-#'   oos_identity_args =
-#'     if(ncol(cheem_basis) >= 2){list(color = "red", size = 5L, shape = 8L)}else
-#'       list(color = "red", size = 1.5, linetype = 2L, length = unit(1, "npc"), alpha = .5),
-#'   ...){ ## Passed to plot.predict_parts()
-#'   .data_else  <- attributes(cheem_basis)$data_else
-#'   .data_oos   <- attributes(cheem_basis)$data_oos
-#'   .class_else <- attributes(cheem_basis)$class_else
-#'   .class_oos  <- attributes(cheem_basis)$class_oos
-#'   .cn <- colnames(cheem_basis)
-#'   
-#'   ## Initialize ggplot_tour()
-#'   gg <- ggplot_tour(basis_array = cheem_basis,data = .data_else)
-#'   
-#'   ## oos projection not done in view_frames
-#'   .oos_proj <- data.frame(.data_oos %*% cheem_basis)
-#'   .a_args <- list() ## Init
-#'   
-#'   ## 2D geom_point and call over oos_args:
-#'   if(ncol(cheem_basis) == 2L){
-#'     .oos_pt_func <- function(...)
-#'       suppressWarnings(geom_point(
-#'         aes_string(x = .cn[1L], y = .cn[2L]), 
-#'         .oos_proj, ...))
-#'     .oos_pt_call <- .oos_pt_func(oos_identity_args)
-#'     .oos_pt_call <- do.call(.oos_pt_func, oos_identity_args)
-#'     
-#'     ## 2D data ggproto
-#'     if(is.null(.class_else) == FALSE) 
-#'       .a_args <- list(color = .obs_class, shape = .obs_class)
-#'     .ggp_data <- list(ggproto_data_points(aes_args = .a_args),
-#'                       ggproto_basis_axes(),
-#'                       labs(x = .cn[1L], y = .cn[2L]),
-#'                       .oos_pt_call)
-#'   }
-#'   
-#'   ## 1D geom_hist over oos args:
-#'   if(ncol(cheem_basis) == 1L){
-#'     .oos_rug_func <- function(...)
-#'       suppressWarnings(geom_rug(
-#'         aes_string(x = .cn[1L]), .oos_proj, ...))
-#'     .oos_rug_call <- do.call(.oos_rug_func, oos_identity_args)
-#'     
-#'     ## 1D data ggproto
-#'     if(is.null(.class_else) == FALSE) 
-#'       .a_args <- list(color = .obs_class, fill = .obs_class)
-#'     .ggp_data <- list(ggproto_data_density1d_rug(aes_args = .a_args),
-#'                       ggproto_basis_axes1d(),
-#'                       labs(x = .cn[1L]),
-#'                       .oos_rug_call)
-#'   }
-#'   
-#'   ## spinifex::view_frame(data_else)
-#'   gg <- gg +
-#'     ggproto_data_background(gridlines = FALSE) +
-#'     .ggp_data +
-#'     theme(axis.title = element_text())
-#'   
-#'   ## Plot(predict_parts(), by patchwork?
-#'   if(show_parts == TRUE){
-#'     require("patchwork")
-#'     .parts <- attributes(cheem_basis)$predict_parts
-#'     gg <- gg + plot(.parts, ...)
-#'   }
-#'   
-#'   return(gg)
-#' }
--- zDev/fastshap.r
+++ zDev/fastshap.r
@@ -1,44 +1,84 @@
+## requires -----
 #install.packages("fastshap")
 require("fastshap")
 require("tictoc")
+require("treeshap")
+require("randomForest")
+require("ranger")    # for fast random forest algorithm
+require("microbenchmark")
 ?fastshap::explain
+set.seed(101) ## Reproducibility, fastshap is spproximate
 
-# Load the sample data; see ?datasets::mtcars for details
-data(mtcars)
+## Data -----
+dat <- DALEX::apartments[, 1:5]
+xdat <- dat[, 2:5] ## -c(m2.price, district))
+y <- dat$m2.price
 
-dat <- DALEX::dragons
-str(dat)
-xdat <- subset(dat, select = -colour)
-test <- dat$colour == dat$colour[5] 
+.rf <- randomForest::randomForest(y ~ ., data = data.frame(y, xdat))
+.ranger <- ranger::ranger(y ~ ., data = data.frame(y, xdat))
 
-#y <- dat$overall
-# Fit a projection pursuit regression model
-tic("RF Fit")
-fit <- randomForest::randomForest(test ~ ., data = data.frame(test, xdat))
+## Summary ----
+print("both treeshap and fastshap only work for continuous predictions.")
+microbenchmark(
+  fit_rf = {randomForest::randomForest(y ~ ., data = data.frame(y, xdat))},
+  fit_ranger = {ranger::ranger(y ~ ., data = data.frame(y, xdat))},
+  fastshap_10_rf = {fastshap::explain(.rf, X = xdat, nsim = 10,
+                                      pred_wrapper = predict)},
+  # fastshap_10_ranger = {fastshap::explain(.ranger, X = xdat, nsim = 10,
+  #                                     pred_wrapper = predict)},
+  treeshap_rf = {treeshap::treeshap(treeshap::randomForest.unify(.rf, xdat), x = xdat)},
+  treeshap_ranger = {treeshap::treeshap(treeshap::ranger.unify(.ranger, xdat), x = xdat)},
+  dalex_rf_1obs = 
+    DALEX::predict_parts_shap(
+      explainer = DALEX::explain(model = .rf, data = xdat, y = y, label = ".rf"),
+      new_observation = xdat[1,, drop =FALSE]),
+  dalex_ranger_1obs =
+    DALEX::predict_parts_shap(
+      explainer = DALEX::explain(model = .ranger, data = xdat, y = y, label = "ranger"),
+      new_observation = xdat[1,, drop =FALSE]),
+  times = 3
+)
+
+
+
+## RF model fit -----
+tic("RF fit")
+fit <- randomForest::randomForest(y ~ ., data = data.frame(y, xdat)) ## Factor y, 2 levels
 toc()
+fit2 <- randomForest::randomForest(mpg ~ ., data = mtcars) ## Continuous y
+f_dat <- tourr::flea[, 1:6]
+f_y <- tourr::flea$species == tourr::flea$species[10]
+fit3 <- randomForest::randomForest(f_y ~ ., data = data.frame(f_y, f_dat)) ## Boolean y
 
-# Compute approximate Shapley values using 10 Monte Carlo simulations
-set.seed(101)  # for reproducibility
-
-## fastshap:: -----
+## pkg: fastshap -----
 tic("Approximate: 10 MC sim")
-(shap <- explain(fit, X = xdat, nsim = 10,
-                 pred_wrapper = predict))
-toc()
+(shap <- explain(rfo, X = xdat, nsim = 10,
+                 pred_wrapper = ranger::predictions)) ## But all NA; because of factor y?
+explain(rfo, X = xdat, pred_wrapper = pfun, nsim = 10)
+toc() ## Approximate: 10 MC sim: 2.8 sec elapsed
+(shap2 <- explain(fit2, X = mtcars[, -1], nsim = 10,
+                 pred_wrapper = predict)) ## Works for continuous, how about boolean tests?
+(shap3 <- explain(fit3, X = f_dat, nsim = 10)) ## fine for continuous, how about binary
+
+rfo <- ranger::ranger(y ~ ., data =  data.frame(y, xdat))
+## have tried using ranger models, and cannot get pred_wrapper to work.
+print("fastshap is DoA, go to treeshap")
 
 
-tic("exact shap: LinearSHAP")
-# Compute exact Shapley (i.e., LinearSHAP) values, not working for RF.
-(shap <- explain(fit, X = subset(dat, select = -colour), exact = TRUE,
-                 pred_wrapper = predict))
-toc()
 
-# Shapley-based plots
-library(ggplot2)
-autoplot(shap)  # Shapley-based importance plot
-autoplot(shap, type = "dependence", feature = "wt", X = mtcars)
-autoplot(shap, type = "contribution", row_num = 1)  # explain first row of X
-
-
-### treeshap ----
+## pkg: treeshap ----
 browseURL("https://github.com/ModelOriented/treeshap")
+?treeshap::treeshap(unified_model, x)
+
+ufit <- randomForest.unify(fit, xdat)
+# Error in randomForest.unify(fit, xdat) : 
+#   Models built on data with categorical features are not supported - please encode them before training.
+urfo <- ranger.unify(rfo, xdat)
+# Error in as.character.factor(x) : malformed factor
+# In addition: Warning message:
+#   In Ops.factor(Prediction, n) : '/' not meaningful for factors
+ufit2 <- randomForest.unify(fit2, mtcars[, -1]) ## Got one!
+ufit3 <- randomForest.unify(fit3, f_dat) ## Got one!
+
+
+?classCenter
